26f2b9cce3a2870a87888fc7461411b285ec0068
Dev Jobalia <webdev.jobalia@gmail.com>
2023-11-07 14:12:46 +0530

    add: Backend Inital Setup 🏗️

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+// import * as dotenv from "dotenv"; // to access enviorment variables
+// import cors from "cors";
+
+// dotenv setup
+// dotenv.config(); // to pull our enviorment varibles from dotEnv file
+
+// initialise express application
+const app = express();
+// add additional middleware
+// app.use(cors());
+// app.use(express.json({ limit: "50mb" }));
+
+// // API ENDPOINTS: to connect our front end side
+// app.use("/api/v1/post", postRoutes);
+// app.use("/api/v1/dalle", dalleRoutes);
+
+// // ROUTE
+app.get("/", async (req, res) => {
+  res.send("Hello from dalle");
+});
+
+// function to run server
+const startServer = async () => {
+  try {
+    // connectDB(process.env.MONGODB_URL);
+    app.listen(3000, () => console.log("Server started on port 3000"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
